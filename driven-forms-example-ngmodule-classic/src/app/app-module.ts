ef56449cdec9961d3638d077bcb9e530668695a9
Alex Cordon <alexcordonvila@gmail.com>
2025-09-28 14:01:02 +0200

    Crear un nuevo componente sign-in y crear nueva ruta al componente

--- driven-forms-example-ngmodule-classic/src/app/app-module.ts
+++ driven-forms-example-ngmodule-classic/src/app/app-module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './Components/login/login.component';
-
+import { SignInComponent } from './Components/sign-in/sign-in.component';
 @NgModule({
   declarations: [
     App,
-    LoginComponent
+    LoginComponent,
+    SignInComponent
 
   ],
   imports: [
--- driven-forms-example-ngmodule-classic/src/app/app-routing-module.ts
+++ driven-forms-example-ngmodule-classic/src/app/app-routing-module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
+import { SignInComponent } from './Components/sign-in/sign-in.component';
 
 const routes: Routes = [
+   // 1. PÁGINA DE INICIO (RAÍZ)
   {
-    path:'',
-    component: LoginComponent 
+    path: '', 
+    redirectTo: 'login',  // Redirige la raíz de la app (ej. /) a /login
+    pathMatch: 'full'     // CRUCIAL: Asegura que solo coincida con la URL completamente vacía.
+  },
+
+  // 2. RUTAS DE NAVEGACIÓN
+  {
+    path: 'login',       // Cuando la URL es /login
+    component: LoginComponent,
+  },
+  {
+    path: 'sign-in',     // Cuando la URL es /sign-in
+    component: SignInComponent,
   }
 ];
 
