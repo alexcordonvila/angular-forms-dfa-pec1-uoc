15c68a08f3bca8867122c40bd48ad33c8656d556
Alex Cordon <alexcordonvila@gmail.com>
2025-09-30 19:47:17 +0200

    Reestructurar proyecto

---
+++ reactive-forms-example/src/app/Components/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { UserDTO } from 'src/app/Models/user.dto';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+/*Declaramos  una  variable  pública  user  y  le  asignamos  el  tipo,  en  este  caso 
+nuestro modelo UserDTO.
+ */
+  user: UserDTO;
+  email: FormControl;
+  password: FormControl;
+  loginForm: FormGroup;
+
+    /*Es importante resaltar que el servicio (provider) FormBuilder debe ser inyectado para 
+    poder construir los formularios haciendo uso de FormGroup y FormControl. */
+  constructor(private formBuilder: FormBuilder){
+    this.user = new UserDTO('dsfgdfg','');
+    this.email = new FormControl(this.user.email);
+    this.password = new FormControl(this.user.password);
+    /*
+    Registramos los FormControl para los campos email y password y 
+    posteriormente los agrupamos dentro del grupo loginForm. Posteriormente en  
+    la vista veremos que esta variable loginForm se la asignaremos a la directiva 
+    [formGroup] del formulario. 
+    */
+    this.loginForm = this.formBuilder.group({
+      email: this.email,
+      password: this.password,
+    });
+  }
+  /* implementamos el método checkLogin que se llamará cuando se haga 
+  submit del formulario. */
+  checkLogin(): void{
+    this.user.email = this.email.value;
+    this.user.password = this.password.value;
+    console.log(
+      'User email-->' +
+      this.user.email +
+      'user password -->' +
+      this.user.password
+    );
+  }
+}
