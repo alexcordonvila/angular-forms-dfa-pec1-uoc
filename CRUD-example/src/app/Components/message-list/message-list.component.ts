ad3af3a63fb8fd7d353f9044fd819af444d7a96a
Alex Cordon <alexcordonvila@gmail.com>
2025-10-04 18:52:34 +0200

    Crear Lista de mensajes con get Messages

--- CRUD-example/src/app/Components/message-list/message-list.component.ts
+++ CRUD-example/src/app/Components/message-list/message-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageDTO } from 'src/app/Models/message.dto';
+import { MessageService } from 'src/app/Services/message.service';
 
 @Component({
   selector: 'app-message-list',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./message-list.component.scss']
 })
 export class MessageListComponent {
+  messages!: MessageDTO[];
+  
+  constructor(private messageService: MessageService, private router: Router){
+    this.loadMessages();
+  }
 
+  private async loadMessages(): Promise<void>{
+    try{
+      this.messages = await this.messageService.getMessages() ?? [];
+    }catch(error: any){
+      this.messageService.errorLog(error);
+    }
+  }
 }
